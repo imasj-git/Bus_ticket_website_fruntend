8c9d0d7df258e0eca7208ab7111fb475803af6b1
imasj-git <joshisaurav044@gamil.com>
2025-01-04 13:12:48 +0545

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { InfoProvider } from "./context/InfoContext.jsx";
+// import BookingIndex from "./core/private/booking";
+const BookingIndex = lazy(() => import("./core/private/booking"));
+
+// import CustomerForm from "./core/private/customer/form";
+const CustomerForm = lazy(() => import("./core/private/customer/form"));
+
+// import CustomerIndex from "./core/private/customer";
+const CustomerIndex = lazy(() => import("./core/private/customer"));
+
+// import Home from "./core/public/home"
+const Home = lazy(() => import("./core/public/home"));
+
+// import Login from "./core/public/login"
+const Login = lazy(() => import("./core/public/login"));
+
+// import LoginCustomer from "./core/public/login-customer"
+// const LoginCustomer = lazy(() => import("./core/public/login-customer"));
+
+// import Register from "./core/public/register"
+const Register = lazy(() => import("./core/public/register"));
+
+// import Layout from "./core/private/layout"
+const Layout = lazy(() => import("./core/private/layout"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const publicRoutes = [
+    {
+      path: "/",
+      element: (
+        <Suspense>
+          <Home />
+        </Suspense>
+      ),
+      // errorElement: <>error</>,
+    },
+    {
+      path: "/login",
+      element: (
+        <Suspense>
+          <Login />
+        </Suspense>
+      ),
+      errorElement: <>error</>,
+    },
+    // {
+    //   path: "/login-customer",
+    //   element: (
+    //     <Suspense>
+    //       <LoginCustomer />
+    //     </Suspense>
+    //   ),
+    //   errorElement: <>error</>,
+    // },
+    {
+      path: "/register",
+      element: (
+        <Suspense>
+          <Register />
+        </Suspense>
+      ),
+      errorElement: <>error</>,
+    },
+    { path: "*", element: <>unauthorized</> },
+  ];
 
+  const privateRoutes = [
+    {
+      path: "/admin",
+      element: (
+        <Suspense>
+          <Layout />
+        </Suspense>
+      ),
+      errorElement: <>error</>,
+      children: [
+        {
+          path: "/admin/customer",
+          element: (
+            <Suspense>
+              <CustomerIndex />
+            </Suspense>
+          ),
+          errorElement: <>error</>,
+        },
+        {
+          path: "/admin/customer/form",
+          element: (
+            <Suspense>
+              <CustomerForm />
+            </Suspense>
+          ),
+          errorElement: <>error</>,
+        },
+        {
+          path: "/admin/booking",
+          element: (
+            <Suspense>
+              <BookingIndex />
+            </Suspense>
+          ),
+          errorElement: <>error</>,
+        },
+      ],
+    },
+    { path: "*", element: <>Page not found</> },
+  ];
+
+  // LOGIN logic TODO
+  const isAdmin = false;
+  const routes = isAdmin ? privateRoutes : publicRoutes;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <InfoProvider> */}
+      <RouterProvider router={createBrowserRouter(routes)} />
+      {/* </InfoProvider> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
